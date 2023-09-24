c82b234b77ac510cae23d224cdbb73f254050f91
Thabet <thabet.rebhi@gmail.com>
2023-09-24 18:04:02 +0100

    update packages with info.plist files

--- Package.swift
+++ Package.swift
@@ -42,27 +42,27 @@ let package = Package(
         .binaryTarget(
             name: "GoogleMaps",
             url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/8.0.0/GoogleMaps.xcframework.zip",
-            checksum: "9b192bd50705bd732d0f75db9b8d16361bf5f8e2bd7df6c16ecced6175e2342e"
+            checksum: "348821919708ede02e0b33336daabe6d6c0611e03ab45355bfaf87885bfccf32"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
             url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/8.0.0/GoogleMapsBase.xcframework.zip",
-            checksum: "ff206e5ba5dae860baa72f25294dd8140cb8427df1c0fbe3205417fabd3a61ba"
+            checksum: "3e6d4d75132effddcc5c00202f1d6eeab8579cc5d75cf43025d2d71c739f41bc"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
             url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/8.0.0/GoogleMapsCore.xcframework.zip",
-            checksum: "dbc48766f1c3d3098d450b87a36411bbf72944d17313687549a3781ba3401a3d"
+            checksum: "d0562aff2a0c183006889548bfd0187c72ddc988e70c1a480ca0e364b7d08c34"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
             url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/8.0.0/GoogleMapsM4B.xcframework.zip",
-            checksum: "b9b5dac29474ad33d0d38e767a4fe4eb7583d02aeca626bb7ef04baa7cbefe79"
+            checksum: "5a807ce813bacacc4a1d0c7d605d42b07edab77a9cb238703bdfdd4a4923bd18"
         ),
         .binaryTarget(
             name: "GooglePlaces",
             url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/8.0.0/GooglePlaces.xcframework.zip",
-            checksum: "71ff9989e5d756f0e83767bf9583cc1e51e76b8c899cace72f87cc9d4b5140e5"
+            checksum: "03e0ad5884cbe70dc7db14bf8e1eaaa953152ed230283ba180bc2f5676b102cf"
         )
     ]
 )
